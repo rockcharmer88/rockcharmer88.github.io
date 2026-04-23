d6a78d394fd6923500c37ef9face6262bf2c6d2d
Brittany B. <bbest0826@live.sunyjefferson.edu>
2026-04-23 18:04:46 -0400

    removed prevent default

--- final/scripts.js
+++ final/scripts.js
@@ -1,6 +1,5 @@
 let form = document.getElementById("contactForm");
 form.addEventListener("submit", function(event) {
-  event.preventDefault();
   let name = document.getElementById("name").value;
   let message = "Thanks, " + name + "! Your message has been received.";
   document.getElementById("formMessage").textContent = message;
